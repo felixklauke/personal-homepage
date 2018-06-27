25b8ce266ff3c2af0f4e2b4dac253e97d123650e
Felix Klauke <info@felix-klauke.de>
2018-06-27 15:36:49 +0200

    Add fresh node.js express project.

--- routes/privacy.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function (req, res, next) {
-    res.render('privacy', {title: 'Felix Klauke - Datenschutz', active: 'privacy'});
-});
-
-module.exports = router;
