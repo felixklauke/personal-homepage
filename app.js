b0606ba7cccd65f5a07099b3d8a6e16398857044
Felix Klauke <info@felix-klauke.de>
2018-06-28 11:11:21 +0200

    Add twig as templating engine and prepare first routing controller for dashboard page.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
 
+/**
+ * Set templating engine to twig.
+ */
+app.set('view engine', 'twig');
+
 let server = app.listen(3002, function () {
     console.log('Listening on ' + server.address().port)
 });
