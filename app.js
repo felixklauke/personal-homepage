80f0da7deebdaf76e04bb704e9245ce443e1fd84
Felix Klauke <info@felix-klauke.de>
2018-07-01 08:16:09 +0200

    Add index page with navigation and footer. Add nodemon for development and begin design.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let sassMiddleware = require('node-sass-middleware');
+let path = require('path');
 let express = require('express');
 let app = express();
 
@@ -5,6 +7,32 @@ let app = express();
  * Set templating engine to twig.
  */
 app.set('view engine', 'twig');
+
+/**
+ * Configure SASS middleware.
+ */
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'assets'),
+    dest: path.join(__dirname, 'public/build'),
+    outputStyle: 'compressed',
+    prefix: '/vendor',
+}));
+
+/**
+ * Routing configuration
+ */
+const dashboardController = require('./routes/dashboardController');
+app.use('/', dashboardController);
+
+/**
+ * Expose directories.
+ */
+app.use('/img', express.static(path.join(__dirname, 'public/img')));
+app.use('/video', express.static(path.join(__dirname, 'public/video')));
+app.use('/vendor', express.static(path.join(__dirname, 'public/build')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/mdbootstrap/js')));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/mdbootstrap/css')));
+app.use('/font', express.static(path.join(__dirname, 'node_modules/mdbootstrap/font')));
 
 let server = app.listen(3002, function () {
     console.log('Listening on ' + server.address().port)
--- routes/dashboardController.js
+++ routes/dashboardController.js
@@ -1,4 +1,8 @@
 let express = require('express');
 let router = express.Router();
 
+router.get('/', function (request, response) {
+    response.render('index.html.twig', {'title': 'Felix Klauke'})
+});
+
 module.exports = router;
